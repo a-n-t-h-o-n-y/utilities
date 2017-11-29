1d57e9d8794be47274470086feab5cfc521b3a7b
Anthony Leedom <anthonym.leedom@gmail.com>
2017-11-29 15:14:43 -0600

    Add Property class.
    Wraps objects, provides get/set members that emit signal when
    value is changed.

---
+++ headers/property.hpp
@@ -0,0 +1,106 @@
+#ifndef UTILITY_PROPERTY_HPP
+#define UTILITY_PROPERTY_HPP
+#include <signals/signals.hpp>
+
+#include <cstdint>
+#include <iostream>
+#include <utility>
+
+namespace utility {
+
+/// Wraps a value and provides get/set members. Setting the value emits a
+/// Signal which passes on the value set.
+template <typename T>
+class Property {
+   public:
+    /// Default construct object of type T.
+    Property() = default;
+
+    /// Construct object of type T that is constructible from type U.
+    template <typename U>
+    Property(U&& value) : value_{std::forward<U>(value)} {}
+
+    /// Return a non-const reference to the property's value.
+    T& get() { return value_; }
+
+    /// Return a const reference to the property's value.
+    const T& get() const { return value_; }
+
+    /// Set value from any U object that can be constructed into a T object.
+    template <typename U>
+    void set(U&& value) {
+        if (value != value_) {
+            value_ = std::forward<U>(value);
+            on_change_(value_);
+        }
+    }
+
+    /// Return a reference to the signal emitted on change of value.
+    sig::Signal<void(const T&)>& on_change() { return on_change_; }
+
+    /// Return a const reference to the signal emitted on change of value.
+    const sig::Signal<void(const T&)>& on_change() const { return on_change_; }
+
+   private:
+    T value_;
+    sig::Signal<void(const T&)> on_change_;
+};
+
+/// std::ostream operator
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const Property<T>& prop) {
+    return os << prop.get();
+}
+
+/// std::istream operator
+template <typename T>
+std::istream& operator>>(std::istream& is, Property<T>& prop) {
+    return is >> prop.get();
+}
+
+// Specialized default constructors. UB if pod type comparison with
+// uninitialized value in set member function.
+template <>
+Property<std::int8_t>::Property() : value_{0} {}
+
+template <>
+Property<std::int16_t>::Property() : value_{0} {}
+
+template <>
+Property<std::int32_t>::Property() : value_{0} {}
+
+template <>
+Property<std::int64_t>::Property() : value_{0} {}
+
+template <>
+Property<std::uint8_t>::Property() : value_{0} {}
+
+template <>
+Property<std::uint16_t>::Property() : value_{0} {}
+
+template <>
+Property<std::uint32_t>::Property() : value_{0} {}
+
+template <>
+Property<std::uint64_t>::Property() : value_{0} {}
+
+template <>
+Property<float>::Property() : value_{0.0} {}
+
+template <>
+Property<double>::Property() : value_{0.0} {}
+
+template <>
+Property<bool>::Property() : value_{false} {}
+
+template <>
+Property<char>::Property() : value_{static_cast<char>(0)} {}
+
+template <>
+Property<char16_t>::Property() : value_{static_cast<char16_t>(0)} {}
+
+template <>
+Property<char32_t>::Property() : value_{static_cast<char32_t>(0)} {}
+
+}  // namespace utility
+#endif  // UTILITY_PROPERTY_HPP
