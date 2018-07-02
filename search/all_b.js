10eb6d4023ee5c668d5a08efb404a5754fe30d75
Anthony Leedom <anthonym.leedom@gmail.com>
2018-07-02 16:31:44 -0500

    Update Docs for addition of duration_view.

--- search/all_b.js
+++ search/all_b.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['property',['Property',['../classutility_1_1Property.html',1,'utility::Property&lt; T &gt;'],['../classutility_1_1Property.html#ade9108007bb5f7e91377719a51f47a08',1,'utility::Property::Property()'],['../classutility_1_1Property.html#addb8744cbc581d3e6e58f8c5ca27e7ad',1,'utility::Property::Property(U &amp;&amp;value)']]]
+  ['property',['Property',['../classutility_1_1Property.html',1,'utility::Property&lt; T &gt;'],['../classutility_1_1Property.html#ade9108007bb5f7e91377719a51f47a08',1,'utility::Property::Property()'],['../classutility_1_1Property.html#acfca91dd4ef8d86193175e612f13d37e',1,'utility::Property::Property(Args &amp;&amp;... args)']]]
 ];
